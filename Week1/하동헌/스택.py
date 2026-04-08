4f76d70399c417e718cd627b280a33d2f38fb97a
하동헌 <149583958+imclaremont@users.noreply.github.com>
2026-04-08 20:12:58 +0900

    import sys

---
+++ Week1/하동헌/스택.py
@@ -0,0 +1,34 @@
+import sys
+
+N = int(sys.stdin.readline())
+stack = []
+result = []
+
+for _ in range(N):
+    cmd = sys.stdin.readline().split()
+
+    if cmd[0] == "push":
+        stack.append(cmd[1])
+
+    elif cmd[0] == "pop":
+        if stack:
+            result.append(stack.pop())
+        else:
+            result.append("-1")
+
+    elif cmd[0] == "size":
+        result.append(str(len(stack)))
+
+    elif cmd[0] == "empty":
+        if stack:
+            result.append("0")
+        else:
+            result.append("1")
+
+    elif cmd[0] == "top":
+        if stack:
+            result.append(stack[-1])
+        else:
+            result.append("-1")
+
+print("\n".join(result))
